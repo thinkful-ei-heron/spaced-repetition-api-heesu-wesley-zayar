64749fe5d87f2bda9af2b0932f65e07aa991c3c3
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-01-03 08:10:31 -0800

    debugging deploy

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DB_URL
     || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder-mifflin@localhost/spaced-repetition'
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
